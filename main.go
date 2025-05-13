9ae2dc7d0725bf17e36eb70dbfdae154f5052034
45uperman <orsuperman@gmail.com>
2025-05-12 19:53:06 -0500

    added commands

--- main.go
+++ main.go
@@ -3,22 +3,87 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/45uperman/gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	supportedCommands map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	callback, ok := c.supportedCommands[cmd.name]
+	if !ok {
+		return fmt.Errorf("invalid command: %s", cmd.name)
+	}
+
+	err := callback(s, cmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.supportedCommands[name] = f
+}
+
 func main() {
-	cfg, err := config.Read()
+	var s state
+	{
+		cfg, err := config.Read()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		s.cfg = &cfg
+	}
+
+	c := commands{supportedCommands: make(map[string]func(*state, command) error)}
+
+	c.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("error: no command given")
+	}
+
+	cmd := command{
+		name: os.Args[1],
+		args: make([]string, 0),
+	}
+
+	if len(os.Args) > 2 {
+		cmd.args = os.Args[2:]
+	}
+
+	err := c.run(&s, cmd)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	cfg.SetUser("Superman")
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatal(err)
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login requires a username")
 	}
 
-	fmt.Printf("db_url: %s\ncurrent_user_name: %s\n", cfg.DBURL, cfg.CurrentUserName)
+	err := s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Logged in as user: %s\n", cmd.args[0])
+
+	return nil
 }
